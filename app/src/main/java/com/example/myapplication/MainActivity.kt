009c2d4aa9ddaab04d6cf16f2bdc4e1558b6782c
boudee <boudispatcher@gmail.com>
2022-12-14 17:30:27 +0800

    FIX: ADDED NEW FEATURE

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-         "DEV 2 Change"
+         "BOU NEW FEATURE"
     }
 }
